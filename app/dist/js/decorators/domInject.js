43f58149152b5f5a8f13ecbb51854ba29510fc50
Lucas Novae Dragojevic <lucas.dragojevic@gmail.com>
2023-03-11 17:32:41 -0300

    Fixed property decorator

--- app/dist/js/decorators/domInject.js
+++ app/dist/js/decorators/domInject.js
@@ -1,8 +1,11 @@
 export function domInject(selector) {
     return function (target, propertyKey) {
+        let element;
         const getter = function () {
-            const element = document.querySelector(selector);
-            console.log(`taking DOM element with selector ${selector} to inject ${propertyKey}`);
+            if (!element) {
+                console.log(`taking DOM element with selector ${selector} to inject ${propertyKey}`);
+                element = document.querySelector(selector);
+            }
             return element;
         };
         Object.defineProperty(target, propertyKey, {
--- app/src/decorators/domInject.ts
+++ app/src/decorators/domInject.ts
@@ -1,8 +1,11 @@
 export function domInject(selector: string) {
     return function(target: any, propertyKey: string) {
+        let element: HTMLElement
         const getter = function() {
-            const element = document.querySelector(selector)
-            console.log(`taking DOM element with selector ${selector} to inject ${propertyKey}`)
+            if (!element) {
+                console.log(`taking DOM element with selector ${selector} to inject ${propertyKey}`)
+                element = document.querySelector(selector) as HTMLElement
+            }
             return element
         }
         Object.defineProperty(target, propertyKey, {
